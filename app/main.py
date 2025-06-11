4ada8f3f3679499a3cafae26a0b52c3f81089c10
rafaeldailymartins <rafaeldaily@outlook.com>
2025-06-11 01:25:10 -0300

    feat: add pytest

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .core.config import settings
-from .core.schemas import GlobalConfig
+from app.core.config import settings
+from app.core.schemas import GlobalConfig
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
