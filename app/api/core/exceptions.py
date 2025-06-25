d3e8a1c0aca60d42154a719030d6aa1f56df5f2c
rafaeldailymartins <rafaeldaily@outlook.com>
2025-06-25 02:12:25 -0300

    feat: add Base HTTP Exception

---
+++ app/api/core/exceptions.py
@@ -0,0 +1,8 @@
+from fastapi import HTTPException
+
+
+class BaseHTTPException(HTTPException):
+    """Base exception for all HTPP exceptions."""
+
+    def __init__(self, status_code: int, message: str):
+        super().__init__(status_code=status_code, detail={"message": message})
