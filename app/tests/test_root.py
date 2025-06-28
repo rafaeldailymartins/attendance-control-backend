b8ddc4a677c17f047ff694951dfd4b9d73f41d01
rafaeldailymartins <rafaeldaily@outlook.com>
2025-06-28 18:08:45 -0300

    ✅ test: add test for health check

--- app/tests/test_root.py
+++ app/tests/test_root.py
@@ -4,3 +4,9 @@ from fastapi.testclient import TestClient
 def test_read_root(client: TestClient):
     response = client.get("/")
     assert response.status_code == 200
+
+
+def test_health_check(client: TestClient):
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json()
