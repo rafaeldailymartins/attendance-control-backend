8317a7b87555e67c44b500964513080ef7fa33b5
rafaeldailymartins <rafaeldaily@outlook.com>
2025-06-28 18:24:50 -0300

    🐛 fix: remove passlib and use bcrypt directly

--- app/api/core/security.py
+++ app/api/core/security.py
@@ -1,7 +1,13 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+import bcrypt
 
 
-def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
+def verify_password(plain_password: str, hashed_password: str):
+    return bcrypt.checkpw(
+        bytes(plain_password, encoding="utf-8"),
+        bytes(hashed_password, encoding="utf-8"),
+    )
+
+
+def get_password_hash(password: str):
+    hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+    return hashed.decode("utf-8")
