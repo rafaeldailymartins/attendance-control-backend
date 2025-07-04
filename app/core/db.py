f63980d2fe57980c7465d99f591c3dc1ca791a30
rafaeldailymartins <rafaeldaily@outlook.com>
2025-07-04 18:02:16 -0300

    ♻️ refactor: remove SessionDep

--- app/core/db.py
+++ app/core/db.py
@@ -1,6 +1,3 @@
-from typing import Annotated
-
-from fastapi import Depends
 from sqlmodel import Session, create_engine
 
 from app.core.config import settings
@@ -18,6 +15,3 @@ def init_db(session: Session):
 def get_session():
     with Session(engine) as session:
         yield session
-
-
-SessionDep = Annotated[Session, Depends(get_session)]
