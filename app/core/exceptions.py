ac787704591ce4a50950faacd1ee3bca12c15102
rafaeldailymartins <rafaeldaily@outlook.com>
2025-07-31 19:27:35 -0300

    ✨ feat: add custom exception handler

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -1,24 +1,32 @@
+from typing import Any
+
 from fastapi import HTTPException, status
 
 
 class BaseHTTPException(HTTPException):
     """Base exception for all HTPP exceptions."""
 
-    def __init__(self, status_code: int, message: str):
-        super().__init__(status_code=status_code, detail={"message": message})
+    def __init__(self, status_code: int, message: str, extra: Any = None):
+        super().__init__(
+            status_code=status_code, detail={"message": message, "extra": extra}
+        )
 
 
 class InternalServerErrorException(BaseHTTPException):
     """Base exception for internal server error."""
 
-    def __init__(self, message: str = "Ocorreu um erro no servidor"):
+    def __init__(self, message: str = "Ocorreu um erro no servidor", extra: Any = None):
         super().__init__(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, message=message
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            message=message,
+            extra=extra,
         )
 
 
 class ForbiddenException(BaseHTTPException):
     """Base exception for internal server error."""
 
-    def __init__(self, message: str = "Acesso negado"):
-        super().__init__(status_code=status.HTTP_403_FORBIDDEN, message=message)
+    def __init__(self, message: str = "Acesso negado", extra: Any = None):
+        super().__init__(
+            status_code=status.HTTP_403_FORBIDDEN, message=message, extra=extra
+        )
